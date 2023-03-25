3bea99ff4cf251f38b89dd96156f1ec186beedaa
Phillip Mugisa <phillipmugisa4@gmail.com>
2023-03-25 13:21:20 +0300

    working on views

--- STATIC/js/scripts.js
+++ STATIC/js/scripts.js
@@ -16,4 +16,17 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     )
 
+    const section_part_toggler = document.querySelectorAll(".section-part-toggler")
+    section_part_toggler.forEach(
+        activator => activator.addEventListener("click", () => {
+            let sectionToShow = document.querySelector(`#${activator.dataset.section}`)
+            console.log(sectionToShow)
+            
+            // close open modals
+            let openSections = document.querySelectorAll(".variable-section-part.inview");
+            openSections.forEach(section => section.classList.remove("inview"))
+
+            sectionToShow.classList.add("inview")
+        })
+    )
 })
